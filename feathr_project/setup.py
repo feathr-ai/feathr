7585e02cd65ef609c58a6e3c3785037d8a370d61
Hangfei Lin <hnlin@linkedin.com>
2022-02-25 14:50:29 -0800

    [feathr] Update setuptools for pypi upload (#11)

--- feathr_project/setup.py
+++ feathr_project/setup.py
@@ -1,8 +1,21 @@
 from setuptools import setup, find_packages
+from pathlib import Path
+
+# Use the README.md in root project
+root_path = Path(__file__).resolve().parent.parent
+long_description = (root_path / "README.md").read_text()
 
 setup(
     name='feathr',
     version='0.1.0',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author_email="frame_dev@linkedin.com",
+    description="An Enterprise-Grade, High Performance Feature Store",
+    url="https://github.com/linkedin/feathr",
+    project_urls={
+        "Bug Tracker": "https://github.com/linkedin/feathr/issues",
+    },
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
@@ -23,5 +36,11 @@ setup(
     ],
     entry_points={
         'console_scripts': ['feathr=feathrcli.cli:cli']
-    }
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7"
 )
