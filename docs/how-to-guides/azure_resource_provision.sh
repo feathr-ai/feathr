819c5ec3fa0a352e0189d5f40a84f25f953f74ff
Xiaoyong Zhu <xiaoyzhu@microsoft.com>
2022-03-04 20:55:46 -0800

    fix usability issues for resource provisioning (#23)

--- docs/how-to-guides/azure_resource_provision.sh
+++ docs/how-to-guides/azure_resource_provision.sh
@@ -1,12 +1,21 @@
 #!/bin/bash          
 
 # Fill in details in this section
-subscription_id=""
-resource_prefix="feathrazuretest3"
+subscription_id="{your_subscription_id}"
+resource_prefix="feathrazuretest"
 location="eastus2"
 synapse_sql_admin_name="cliuser1"
-synapse_sql_admin_password=""
+synapse_sql_admin_password="{your_admin_password}"
 synapse_sparkpool_name="spark31"
+
+# You don't have to modify the names below
+service_principal_name="$resource_prefix"sp
+resoruce_group_name="$resource_prefix"rg
+storage_account_name="$resource_prefix"sto
+storage_file_system_name="$resource_prefix"fs
+synapse_workspace_name="$resource_prefix"spark
+redis_cluster_name="$resource_prefix"redis
+purview_account_name="$resource_prefix"purview
 
 # detect whether az cli is installed or not
 if ! [ -x "$(command -v az)" ]; then
@@ -14,22 +23,17 @@ if ! [ -x "$(command -v az)" ]; then
   exit 1
 fi
 
+az upgrade --all true --yes
 # login if required
 az account get-access-token
 if [[ $? == 0 ]]; then
   echo "Logged in, using current subscriptions "
 else
   echo "Logging in via az login..."
-  az login
+  az login --use-device-code
 fi
 
-service_principal_name="$resource_prefix-sp"
-resoruce_group_name="$resource_prefix-rg"
-storage_account_name="$resource_prefix"storage
-storage_file_system_name="$resource_prefix"fs
-synapse_workspace_name="$resource_prefix"synapse
-redis_cluster_name="$resource_prefix"redis
-purview_account_name="$resource_prefix-purview"
+
 
 echo "Setting subscription to $subscription_id, Creating $service_principal_name service principal"
 az account set -s $subscription_id
@@ -64,10 +68,10 @@ az synapse spark pool create --name $synapse_sparkpool_name --workspace-name $sy
 # depending on your preference, you can set a narrow range of IPs (like below) or a broad range of IPs to allow client access to Synapse clusters
 external_ip=$(curl -s http://whatismyip.akamai.com/)
 echo "External IP is: ${external_ip}. Adding it to firewall rules" 
-az synapse workspace firewall-rule create --name allowAll --workspace-name $synapse_workspace_name --resource-group $resoruce_group_name --start-ip-address "$external_ip" --end-ip-address "$external_ip"
+az synapse workspace firewall-rule create --name allowCurrentIP --workspace-name $synapse_workspace_name --resource-group $resoruce_group_name --start-ip-address "$external_ip" --end-ip-address "$external_ip"
 
 # sleep for a few seconds for the chagne to take effect
-sleep 2
+sleep 30
 az synapse role assignment create --workspace-name $synapse_workspace_name --role "Synapse Contributor" --assignee $service_principal_name
 
 echo "Verify if the assignment is successful or not:"
@@ -90,7 +94,7 @@ az purview account create --location $location --account-name $purview_account_n
 
 # this is completely optional. It will download some demo NYC data and upload it to the default storage account, to make the setup experience smoother
 echo "preparing data"
-wget -O /tmp/green_tripdata_2020-04.csv https://s3.amazonaws.com/nyc-tlc/trip+data/green_tripdata_2020-04.csv
+curl https://s3.amazonaws.com/nyc-tlc/trip+data/green_tripdata_2020-04.csv --output /tmp/green_tripdata_2020-04.csv
 az storage fs file upload --account-name $storage_account_name --file-system $storage_file_system_name --path demo_data/green_tripdata_2020-04.csv --source /tmp/green_tripdata_2020-04.csv --auth-mode login
 
 # show output again
