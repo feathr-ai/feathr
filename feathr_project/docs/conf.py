f61f559ec0b90a5043ac13510fac724ac2c1ebaf
Hangfei Lin <hnlin@linkedin.com>
2022-04-29 16:06:33 -0700

    Update conf.py

--- feathr_project/docs/conf.py
+++ feathr_project/docs/conf.py
@@ -26,7 +26,7 @@ author = 'Feathr Community'
 # The short X.Y version
 version = '0.4'
 # The full version, including alpha/beta/rc tags
-release = '0.4_azure_alpha'
+release = '0.4'
 
 
 # -- General configuration ---------------------------------------------------
