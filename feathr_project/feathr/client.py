7b9aad0a8a69173ec00595eba1a2497954413695
Chang Yong Lik <theeahlag@gmail.com>
2022-05-05 15:40:49 +0900

    Removed typo and added a test case
    Signed-off-by: Chang Yong Lik <theeahlag@gmail.com>

--- feathr_project/feathr/client.py
+++ feathr_project/feathr/client.py
@@ -6,7 +6,6 @@ from datetime import datetime, timedelta
 from pathlib import Path
 from typing import Dict, List, Optional, Union
 from pprint import pprint
-from xmlrpc.client import Boolean
 
 import redis
 from azure.identity import DefaultAzureCredential
--- feathr_project/test/test_feature_materialization.py
+++ feathr_project/test/test_feature_materialization.py
@@ -1,9 +1,24 @@
+import os
 from datetime import datetime, timedelta
+from pathlib import Path
 
 from feathr._materialization_utils import _to_materialization_config
 from feathr import (BackfillTime, MaterializationSettings)
 from feathr import RedisSink
+from feathr.anchor import FeatureAnchor
+from feathr.dtype import FLOAT, INT32, ValueType
+from feathr.feature import Feature
+from feathr.source import  HdfsSource
+from feathr.typed_key import TypedKey
+from pyspark.sql import DataFrame
+from pyspark.sql.functions import col
 
+from test_fixture import basic_test_setup
+
+def add_new_fare_amount(df: DataFrame) -> DataFrame:
+    df = df.withColumn("fare_amount_new", col("fare_amount") + 8000000)
+
+    return df
 
 def test_feature_materialization_config():
     backfill_time = BackfillTime(start=datetime(2020, 5, 20), end=datetime(2020, 5,20), step=timedelta(days=1))
@@ -57,3 +72,49 @@ def test_feature_materialization_now_schedule():
     assert expected.year == date.year
     assert expected.month == date.month
     assert expected.day == date.day
+
+def test_feature_materialization_now_schedule():
+    """Test back fill cutoff time without backfill."""
+    settings = MaterializationSettings("", [], [])
+    date = settings.get_backfill_cutoff_time()[0]
+    expected = datetime.now()
+    assert expected.year == date.year
+    assert expected.month == date.month
+    assert expected.day == date.day
+
+def test_build_feature_pretty_print_flag():
+    """
+    Test non-SWA feature gen with preprocessing
+    """
+    test_workspace_dir = Path(__file__).parent.resolve() / "test_user_workspace"
+
+    client = basic_test_setup(os.path.join(test_workspace_dir, "feathr_config.yaml"))
+
+    batch_source = HdfsSource(name="nycTaxiBatchSource_add_new_fare_amount",
+                              path="wasbs://public@azurefeathrstorage.blob.core.windows.net/sample_data/green_tripdata_2020-04.csv",
+                              preprocessing=add_new_fare_amount,
+                              event_timestamp_column="lpep_dropoff_datetime",
+                              timestamp_format="yyyy-MM-dd HH:mm:ss")
+
+    pickup_time_as_id = TypedKey(key_column="lpep_pickup_datetime",
+                                 key_column_type=ValueType.INT32,
+                                 description="location id in NYC",
+                                 full_name="nyc_taxi.location_id")
+
+    features = [
+        Feature(name="f_is_long_trip_distance",
+                key=pickup_time_as_id,
+                feature_type=FLOAT,
+                transform="fare_amount_new"),
+        Feature(name="f_day_of_week",
+                key=pickup_time_as_id,
+                feature_type=INT32,
+                transform="dayofweek(lpep_dropoff_datetime)"),
+    ]
+
+    regular_anchor = FeatureAnchor(name="request_features_add_new_fare_amount",
+                                   source=batch_source,
+                                   features=features,
+                                   )
+
+    client.build_features(anchor_list=[regular_anchor], pprint_flag=True)
