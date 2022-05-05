11f8ecb790b47a7dcc6524f32d73f3c9578745ba
blrchen <blrchen@users.noreply.github.com>
2022-05-05 11:13:08 -0700

    Feathr Web UI MVP Prototype (#216)
    * Init react based feathr ui code (#1)
    
    * Enables continues integration and deployment to cloud for ui app (#3)
    
    This PR enables continues integration and deployment to cloud for ui app
    
    - Manually create azure web app under ACE subscription resource group feathr-ui-test
    - Create github action workflow to trigger deployment on main branch push or pull request
    - Deploy policy
       - Main branch will be deployed to https://thankful-mud-0addb6c00.1.azurestaticapps.net/
       - Each pull request will be deployed to a staging environment, the url will be generated dynamically n each PR's comment.
    
    * Add UI landing page
    
    * Workaround for CI treat warning as error
    
    * Enable client side route in Azure Static Web App
    
    * Code clean up
    
    * Code clean up
    
    * Enable eslint rules
    
    * Update README.md
    
    * Fix CI error
    
    * Add data source mockup page
    
    * Enable Azure AD login
    
    * Complete api integration
    
    * Complete api integration
    
    * Show lineage in feature detail page
    
    * Resolve demo review comments
    
    * Update page title
    
    * Add lineage UI page
    
    * Update Web UI CICD file name and secret name
    
    * Workaround for PR from forked branch can not access main repo's secret
    
    * Workaround for PR from forked branch can not access main repo's secret
    
    * Resolve code review comments
    
    * Test use main repo's secret
    
    * Revert "Test use main repo's secret"
    
    This reverts commit 0ca7f07065f688d3e9baae9fb41c07c4b97ed1c9.
    
    * Remove two more hard codings

---
+++ ui/src/router/routes.tsx
@@ -0,0 +1,57 @@
+import React, { Suspense } from 'react';
+import { BrowserRouter, Route, Switch, withRouter } from 'react-router-dom';
+import { Layout } from 'antd';
+import { QueryClient, QueryClientProvider } from "react-query";
+import { Configuration, InteractionType, PublicClientApplication } from "@azure/msal-browser";
+import { MsalAuthenticationTemplate, MsalProvider } from "@azure/msal-react";
+import Header from '../components/header/header';
+import SideMenu from '../components/sidemenu/siteMenu';
+import Features from "../pages/feature/features";
+import NewFeature from "../pages/feature/newFeature";
+import FeatureDetails from "../pages/feature/featureDetails";
+import DataSources from "../pages/dataSource/dataSources";
+import FeatureLineage from "../pages/feature/featureLineage";
+
+type Props = {};
+const queryClient = new QueryClient();
+const AAD_APP_CLIENT_ID = "c8d4653e-ddaf-4154-a342-01e38ce5a4a0";
+const AAD_APP_AUTHORITY = "https://login.microsoftonline.com/72f988bf-86f1-41af-91ab-2d7cd011db47";
+const msalConfig: Configuration = {
+  auth: {
+    clientId: AAD_APP_CLIENT_ID,
+    authority: AAD_APP_AUTHORITY,
+    redirectUri: window.location.origin
+  }
+};
+const pca = new PublicClientApplication(msalConfig);
+const Routes: React.FC<Props> = () => {
+  return (
+    <MsalProvider instance={ pca }>
+      <MsalAuthenticationTemplate interactionType={ InteractionType.Redirect }>
+        <QueryClientProvider client={ queryClient }>
+          <BrowserRouter>
+            <Layout style={ { minHeight: "100vh" } }>
+              <SideMenu />
+              <Layout>
+                <Header />
+                <Switch>
+                  <Suspense fallback={ <div /> }>
+                    <Route exact={ true } path="/dataSources" component={ withRouter(DataSources) } />
+                    <Route exact={ true } path="/features" component={ withRouter(Features) } />
+                    <Route exact={ true } path="/new-feature" component={ withRouter(NewFeature) } />
+                    <Route exact={ true } path="/features/:qualifiedName" component={ withRouter(FeatureDetails) } />
+                    <Route exact={ true } path="/features/:qualifiedName/lineage" component={ withRouter(FeatureLineage) } />
+                    {/* {publicRoutes} */ }
+                    {/* <Route component={NotFound} /> */ }
+                  </Suspense>
+                </Switch>
+              </Layout>
+            </Layout>
+          </BrowserRouter>
+        </QueryClientProvider>
+      </MsalAuthenticationTemplate>
+    </MsalProvider>
+  );
+}
+
+export default Routes;
