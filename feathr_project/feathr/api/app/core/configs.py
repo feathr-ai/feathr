795f1359d163e033cd82aa8683a3f95e1fbbe61e
Blair Chen <blrchen@hotmail.com>
2022-05-11 18:52:06 +0800

    Update api version

--- feathr_project/feathr/api/app/core/configs.py
+++ feathr_project/feathr/api/app/core/configs.py
@@ -1,7 +1,7 @@
 import logging
 import os
 
-API_Version = "/V1"
+API_Version = "/v"
 log_level = os.getenv("logLevel", "INFO")
 app_insights_connection_string = os.getenv("APPLICATIONINSIGHTS_CONNECTION_STRING")
 formatter = logging.Formatter("[%(asctime)s] [%(name)s:%(lineno)s - %(funcName)5s()] %(levelname)s - %(message)s")
