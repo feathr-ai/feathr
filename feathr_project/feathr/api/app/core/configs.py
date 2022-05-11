303d3cbb22f9cbdf1f57260d7d0f485fc55f413f
Blair Chen <blrchen@hotmail.com>
2022-05-11 20:45:15 +0800

    Fix typo

--- feathr_project/feathr/api/app/core/configs.py
+++ feathr_project/feathr/api/app/core/configs.py
@@ -1,7 +1,7 @@
 import logging
 import os
 
-API_Version = "/v"
+API_Version = "/v1"
 log_level = os.getenv("logLevel", "INFO")
 app_insights_connection_string = os.getenv("APPLICATIONINSIGHTS_CONNECTION_STRING")
 formatter = logging.Formatter("[%(asctime)s] [%(name)s:%(lineno)s - %(funcName)5s()] %(levelname)s - %(message)s")
