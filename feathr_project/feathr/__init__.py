7eb07af24444838d1e10f562b815742a21e56c41
Xiaoyong Zhu <xiaoyzhu@outlook.com>
2022-05-18 17:05:19 -0700

    Update __init__.py

--- feathr_project/feathr/__init__.py
+++ feathr_project/feathr/__init__.py
@@ -2,6 +2,7 @@ from .client import FeathrClient
 from .definition.feature_derivations import *
 from .definition.anchor import *
 from .definition.feature import *
+from .definition.feathrconfig import *
 from .definition.transformation import *
 from .definition.dtype import *
 from .definition.source import *
@@ -11,11 +12,15 @@ from .definition.sink import *
 from .definition.query_feature_list import *
 from .definition.lookup_feature import *
 from .definition.aggregation import *
+from .definition.settings import *
 from .utils.job_utils import *
 from .api.app.core.feathr_api_exception import *
+
+# skipped class as they are internal methods:
+# RepoDefinitions, HoconConvertible, 
 # expose the modules so docs can build
 # referencee： https://stackoverflow.com/questions/15115514/how-do-i-document-classes-without-the-module-name/31594545#31594545
 __all__ = ['FeatureJoinJobParams', 'FeatureGenerationJobParams', 'FeathrClient', 'DerivedFeature', 'FeatureAnchor', 'Feature', 'ValueType', 'WindowAggTransformation', 'TypedKey', 'DUMMYKEY', 'BackfillTime', 'MaterializationSettings', 'RedisSink', 'FeatureQuery', 'LookupFeature', 'Aggregation', 'get_result_df', 'AvroJsonSchema', 'Source', 'InputContext', 'HdfsSource', 'KafkaConfig',
- 'KafKaSource', 'ValueType', 'BooleanFeatureType', 'Int32FeatureType', 'Int64FeatureType', 'FloatFeatureType', 'DoubleFeatureType', 'StringFeatureType', 'BytesFeatureType', 'FloatVectorFeatureType', 'Int32VectorFeatureType', 'Int64VectorFeatureType', 'DoubleVectorFeatureType', 'FeatureNameValidationError']
+ 'KafKaSource', 'ValueType', 'BooleanFeatureType', 'Int32FeatureType', 'Int64FeatureType', 'FloatFeatureType', 'DoubleFeatureType', 'StringFeatureType', 'BytesFeatureType', 'FloatVectorFeatureType', 'Int32VectorFeatureType', 'Int64VectorFeatureType', 'DoubleVectorFeatureType', 'FeatureNameValidationError', 'ObservationSettings']
 
 
