d6487131f76a202381a75ac165ff97308cd837a1
Chen Xu <windoze@0d0a.com>
2022-06-18 00:18:45 +0800

    df loader fallback

--- src/main/scala/com/linkedin/feathr/offline/source/dataloader/hdfs/FileFormat.scala
+++ src/main/scala/com/linkedin/feathr/offline/source/dataloader/hdfs/FileFormat.scala
@@ -2,6 +2,7 @@ package com.linkedin.feathr.offline.source.dataloader.hdfs
 
 import com.linkedin.feathr.common.exception.FeathrException
 import com.linkedin.feathr.offline.source.dataloader._
+import com.linkedin.feathr.offline.source.dataloader.jdbc.JdbcUtils
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.DataFrame
 
@@ -91,6 +92,9 @@ object FileFormat {
         ss.read.format(ORC_DATASOURCE).load(existingHdfsPaths: _*)
       case PARQUET =>
         ss.read.format(PARQUET).load(existingHdfsPaths: _*)
+      case JDBC =>
+        // TODO: We should stop using JDBC URL as simple path, otherwise the code will be full of such hack
+        JdbcUtils.loadDataFrame(ss, existingHdfsPaths.head)
       case _ =>
         // Allow dynamic config of the file format if users want to use one
         if (ss.conf.getOption("spark.feathr.inputFormat").nonEmpty) ss.read.format(ss.conf.get("spark.feathr.inputFormat")).load(existingHdfsPaths: _*)
