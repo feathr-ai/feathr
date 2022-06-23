35d1e40634ef6ec8567de33c19ba3686112706cb
Chen Xu <windoze@0d0a.com>
2022-06-23 21:02:17 +0800

    API impl

--- registry/sql-registry/main.py
+++ registry/sql-registry/main.py
@@ -1,10 +1,11 @@
 import os
 from typing import Optional
+from uuid import UUID
 from fastapi import APIRouter, FastAPI, HTTPException
 from starlette.middleware.cors import CORSMiddleware
 from registry import *
 from registry.db_registry import DbRegistry
-from registry.models import EntityType
+from registry.models import AnchorDef, AnchorFeatureDef, DerivedFeatureDef, EntityType, ProjectDef, SourceDef, to_snake
 
 rp = "/"
 try:
@@ -21,11 +22,12 @@ router = APIRouter()
 
 # Enables CORS
 app.add_middleware(CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+                   allow_origins=["*"],
+                   allow_credentials=True,
+                   allow_methods=["*"],
+                   allow_headers=["*"],
+                   )
+
 
 @router.get("/projects")
 def get_projects() -> list[str]:
@@ -54,7 +56,8 @@ def get_project_features(project: str, keyword: Optional[str] = None) -> list:
         features = registry.get_entities(feature_ids)
         return list([e.to_dict() for e in features])
     else:
-        efs = registry.search_entity(keyword, [EntityType.AnchorFeature, EntityType.DerivedFeature])
+        efs = registry.search_entity(
+            keyword, [EntityType.AnchorFeature, EntityType.DerivedFeature])
         feature_ids = [ef.id for ef in efs]
         features = registry.get_entities(feature_ids)
         return list([e.to_dict() for e in features])
@@ -64,7 +67,8 @@ def get_project_features(project: str, keyword: Optional[str] = None) -> list:
 def get_feature(feature: str) -> dict:
     e = registry.get_entity(feature)
     if e.entity_type not in [EntityType.DerivedFeature, EntityType.AnchorFeature]:
-        raise HTTPException(status_code=404, detail=f"Feature {feature} not found")
+        raise HTTPException(
+            status_code=404, detail=f"Feature {feature} not found")
     return e
 
 
@@ -74,4 +78,34 @@ def get_feature_lineage(feature: str) -> dict:
     return lineage.to_dict()
 
 
-app.include_router(prefix = rp, router=router)
+@router.post("/projects")
+def new_project(definition: dict) -> UUID:
+    return registry.create_project(ProjectDef(**to_snake(definition)))
+
+
+@router.post("/projects/{project}/datasources")
+def new_project_datasource(project: str, definition: dict) -> UUID:
+    project_id = registry.get_entity_id(project)
+    return registry.create_project_datasource(project_id, SourceDef(**to_snake(definition)))
+
+
+@router.post("/projects/{project}/anchors")
+def new_project_anchor(project: str, definition: dict) -> UUID:
+    project_id = registry.get_entity_id(project)
+    return registry.create_project_anchor(project_id, AnchorDef(**to_snake(definition)))
+
+
+@router.post("/projects/{project}/anchors/{anchor}/features")
+def new_project_anchor_feature(project: str, anchor: str, definition: dict) -> UUID:
+    project_id = registry.get_entity_id(project)
+    anchor_id = registry.get_entity_id(anchor)
+    return registry.create_project_anchor_feature(project_id, anchor_id, AnchorFeatureDef(**to_snake(definition)))
+
+
+@router.post("/projects/{project}/derivedfeatures")
+def new_project_derived_feature(project: str, definition: dict) -> UUID:
+    project_id = registry.get_entity_id(project)
+    return registry.create_project_derived_feature(project_id, DerivedFeatureDef(**to_snake(definition)))
+
+
+app.include_router(prefix=rp, router=router)
