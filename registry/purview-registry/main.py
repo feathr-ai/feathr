afb8783a5e397806299137d79976d357e1ec7671
Yihui Guo <yihgu@microsofot.com>
2022-07-05 18:34:41 +0800

    Fix search issue

--- registry/purview-registry/main.py
+++ registry/purview-registry/main.py
@@ -30,7 +30,6 @@ def to_camel(s):
     elif isinstance(s, dict):
         return dict([(to_camel(k), to_camel(s[k]) if isinstance(s[k],dict) or isinstance(s[k],list) else s[k]) for k in s])
 
-# os.environ['PURVIEW_NAME'] = "feathrazuretest3-purview1"
 purview_name = os.environ["PURVIEW_NAME"]
 registry = PurviewRegistry(purview_name)
 app = FastAPI()
@@ -45,17 +44,17 @@ app.add_middleware(CORSMiddleware,
                    )
 
 
-@router.get("/projects")
+@router.get("/projects",tags=["Project"])
 def get_projects() -> list[str]:
     return registry.get_projects()
 
 
-@router.get("/projects/{project}")
+@router.get("/projects/{project}",tags=["Project"])
 def get_projects(project: str) -> dict:
     return to_camel(registry.get_project(project).to_dict())
 
 
-@router.get("/projects/{project}/datasources")
+@router.get("/projects/{project}/datasources",tags=["Project"])
 def get_project_datasources(project: str) -> list:
     p = registry.get_entity(project,True)
     source_ids = [s.id for s in p.attributes.sources]
@@ -63,9 +62,9 @@ def get_project_datasources(project: str) -> list:
     return list([to_camel(e.to_dict()) for e in sources])
 
 
-@router.get("/projects/{project}/features")
+@router.get("/projects/{project}/features",tags=["Project"])
 def get_project_features(project: str, keyword: Optional[str] = None) -> list:
-    if keyword is None:
+    if keyword is None or keyword.strip()=='':
         p = registry.get_entity(project,True)
         feature_ids = [s.id for s in p.attributes.anchor_features] + \
             [s.id for s in p.attributes.derived_features]
@@ -73,13 +72,13 @@ def get_project_features(project: str, keyword: Optional[str] = None) -> list:
         return list([to_camel(e.to_dict()) for e in features])
     else:
         efs = registry.search_entity(
-            keyword, [EntityType.AnchorFeature, EntityType.DerivedFeature])
+            keyword, [EntityType.AnchorFeature, EntityType.DerivedFeature],project=project)
         feature_ids = [ef.id for ef in efs]
         features = registry.get_entities(feature_ids)
         return list([to_camel(e.to_dict()) for e in features])
 
 
-@router.get("/features/{feature}")
+@router.get("/features/{feature}",tags=["Feature"])
 def get_feature(feature: str) -> dict:
     e = registry.get_entity(feature,True)
     if e.entity_type not in [EntityType.DerivedFeature, EntityType.AnchorFeature]:
@@ -88,33 +87,33 @@ def get_feature(feature: str) -> dict:
     return to_camel(e.to_dict())
 
 
-@router.get("/features/{feature}/lineage")
+@router.get("/features/{feature}/lineage",tags=["Feature"])
 def get_feature_lineage(feature: str) -> dict:
     lineage = registry.get_lineage(feature)
     return to_camel(lineage.to_dict())
 
 
-@router.post("/projects")
+@router.post("/projects",tags=["Project"])
 def new_project(definition: dict) -> UUID:
     id = registry.create_project(ProjectDef(**to_snake(definition)))
     return {"guid": str(id)}
 
 
-@router.post("/projects/{project}/datasources")
+@router.post("/projects/{project}/datasources",tags=["Project"])
 def new_project_datasource(project: str, definition: dict) -> UUID:
     project_id = registry.get_entity_id(project)
     id = registry.create_project_datasource(project_id, SourceDef(**to_snake(definition)))
     return {"guid": str(id)}
 
 
-@router.post("/projects/{project}/anchors")
+@router.post("/projects/{project}/anchors",tags=["Project"])
 def new_project_anchor(project: str, definition: dict) -> UUID:
     project_id = registry.get_entity_id(project)
     id = registry.create_project_anchor(project_id, AnchorDef(**to_snake(definition)))
     return {"guid": str(id)}
 
 
-@router.post("/projects/{project}/anchors/{anchor}/features")
+@router.post("/projects/{project}/anchors/{anchor}/features",tags=["Project"])
 def new_project_anchor_feature(project: str, anchor: str, definition: dict) -> UUID:
     project_id = registry.get_entity_id(project)
     anchor_id = registry.get_entity_id(anchor)
@@ -122,7 +121,7 @@ def new_project_anchor_feature(project: str, anchor: str, definition: dict) -> U
     return {"guid": str(id)}
 
 
-@router.post("/projects/{project}/derivedfeatures")
+@router.post("/projects/{project}/derivedfeatures",tags=["Project"])
 def new_project_derived_feature(project: str, definition: dict) -> UUID:
     project_id = registry.get_entity_id(project)
     id = registry.create_project_derived_feature(project_id, DerivedFeatureDef(**to_snake(definition)))
