9ceb6ec492e63af4fad73701e009027524dc38eb
DJ Kim <donegjookim@gmail.com>
2022-07-21 07:58:57 -0800

    Add prettier and pre-commit hook (#483)

--- ui/src/pages/management/management.tsx
+++ ui/src/pages/management/management.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import { Card, Typography } from 'antd';
-import UserRoles from '../../components/userRoles';
+import React from "react";
+import { Card, Typography } from "antd";
+import UserRoles from "../../components/userRoles";
 
 const { Title } = Typography;
 
 const Management: React.FC = () => {
   return (
     <div className="page">
-      <Card style={ { minWidth: '1000px' } }>
-        <Title level={ 3 }>Management</Title>
+      <Card style={{ minWidth: "1000px" }}>
+        <Title level={3}>Management</Title>
         <UserRoles />
       </Card>
     </div>
