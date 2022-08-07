a42b927de3610fb952b44a2d47cb57f765968484
Xiaoyong Zhu <xiaoyongzhu@users.noreply.github.com>
2022-08-06 23:33:07 -0700

    Update _preprocessing_pyudf_manager.py

--- feathr_project/feathr/udf/_preprocessing_pyudf_manager.py
+++ feathr_project/feathr/udf/_preprocessing_pyudf_manager.py
@@ -170,6 +170,7 @@ feature_names_funcs = {
             client_udf_repo_path = os.path.join(local_workspace_dir, FEATHR_CLIENT_UDF_FILE_NAME)
             # write pyspark_driver_template_abs_path and then client_udf_repo_path
             filenames = [pyspark_driver_template_abs_path, client_udf_repo_path]
+            # will always overwrite the old files so it's fine not removing it
             with open(pyspark_driver_path, 'w') as outfile:
                 for fname in filenames:
                     with open(fname) as infile:
