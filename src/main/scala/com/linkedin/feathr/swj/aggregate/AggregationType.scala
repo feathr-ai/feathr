382de8bde2b47de04ffefc8b0aac95cdfd24214c
esadler-hbo <97543480+esadler-hbo@users.noreply.github.com>
2022-08-22 13:12:21 -0400

    Add COUNT_DISTINCT aggregation (#594)
    * scala COUNT_DISTINCT aggregation
    
    * cr change comment table to match expected results
    
    * cr - fix comment
    
    * cr - remove showing df

--- src/main/scala/com/linkedin/feathr/swj/aggregate/AggregationType.scala
+++ src/main/scala/com/linkedin/feathr/swj/aggregate/AggregationType.scala
@@ -2,5 +2,5 @@ package com.linkedin.feathr.swj.aggregate
 
 object AggregationType extends Enumeration {
   type AggregationType = Value
-  val SUM, COUNT, AVG, MAX, TIMESINCE, LATEST, DUMMY, MIN, MAX_POOLING, MIN_POOLING, AVG_POOLING, SUM_POOLING = Value
+  val SUM, COUNT, COUNT_DISTINCT, AVG, MAX, TIMESINCE, LATEST, DUMMY, MIN, MAX_POOLING, MIN_POOLING, AVG_POOLING, SUM_POOLING = Value
 }
