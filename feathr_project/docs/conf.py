b23bd4718b941fedd0c81df9ba640dabaa512322
Blair Chen <blrchen@users.noreply.github.com>
2022-08-26 10:32:27 +0800

    Bump versions to 0.7.0 (#607)

--- feathr_project/docs/conf.py
+++ feathr_project/docs/conf.py
@@ -24,9 +24,9 @@ copyright = '2022, Feathr Community'
 author = 'Feathr Community'
 
 # The short X.Y version
-version = '0.6'
+version = '0.7'
 # The full version, including alpha/beta/rc tags
-release = '0.6'
+release = '0.7'
 
 
 # -- General configuration ---------------------------------------------------
