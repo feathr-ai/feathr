cdc45e8690aa53e695f9572834c16c27a46f75a7
Chen Xu <windoze@0d0a.com>
2022-08-28 19:15:48 +0800

    CI test

--- feathr_project/test/test_azure_spark_e2e.py
+++ feathr_project/test/test_azure_spark_e2e.py
@@ -1,7 +1,7 @@
 import os
 from datetime import datetime, timedelta
 from pathlib import Path
-from feathr.definition.sink import CosmosDbSink
+from feathr.definition.sink import CosmosDbSink, ElasticSearchSink
 
 import pytest
 from click.testing import CliRunner
@@ -262,6 +262,36 @@ def test_feathr_materialize_to_cosmosdb():
                                            "f_location_avg_fare", "f_location_max_fare"],
                                        backfill_time=backfill_time)
     client.materialize_features(settings)
+    # assuming the job can successfully run; otherwise it will throw exception
+    client.wait_job_to_finish(timeout_sec=Constants.SPARK_JOB_TIMEOUT_SECONDS)
+
+
+@pytest.mark.skip(reason="Marked as skipped as we need to setup resources for this test")
+def test_feathr_materialize_to_es():
+    """
+    Test FeathrClient() CosmosDbSink.
+    """
+    test_workspace_dir = Path(
+        __file__).parent.resolve() / "test_user_workspace"
+    # os.chdir(test_workspace_dir)
+
+    client: FeathrClient = basic_test_setup(os.path.join(test_workspace_dir, "feathr_config.yaml"))
+
+    backfill_time = BackfillTime(start=datetime(
+        2020, 5, 20), end=datetime(2020, 5, 20), step=timedelta(days=1))
+
+    now = datetime.now()
+    index = ''.join(['feathrazure_cijob_materialize_','_', str(now.minute), '_', str(now.second), ""])
+    sink = ElasticSearchSink(name='es1', host='somenode:9200', index=index, ssl=True, auth=True)
+    settings = MaterializationSettings("nycTaxiTable",
+                                       sinks=[sink],
+                                       feature_names=[
+                                           "f_location_avg_fare", "f_location_max_fare"],
+                                       backfill_time=backfill_time)
+    client.materialize_features(settings)
+    # Set user and password before submitting job
+    # os.environ[f"es1_USER"] = "some_user"
+    # os.environ[f"es1_PASSWORD"] = "some_password"
     # assuming the job can successfully run; otherwise it will throw exception
     client.wait_job_to_finish(timeout_sec=Constants.SPARK_JOB_TIMEOUT_SECONDS)
 
