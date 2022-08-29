0e5537de913a16f7055b23ab705303ee0316c0b9
Richin Jain <rijai@microsoft.com>
2022-08-29 06:40:28 -0700

    Adding tty options

--- .github/decodekey.sh
+++ .github/decodekey.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 gpg --version
 echo "Hello, this is decode file"
+GPG_TTY=$(tty)
 GPG_OPTIONS="--no-show-photos --pinentry-mode loopback"
 echo $PGP_SECRET | base64 --decode | gpg  --import
