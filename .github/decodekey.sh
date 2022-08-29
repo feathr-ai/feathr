5347c64758e7a040101a314f48e30a70dbabdcd2
Richin Jain <rijai@microsoft.com>
2022-08-29 06:49:47 -0700

    Bringing TTY back

--- .github/decodekey.sh
+++ .github/decodekey.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 gpg --version
 echo "Hello, this is decode file"
-GPG_TTY=$(tty)
+export GPG_TTY=$(tty)
 GPG_OPTIONS="--no-show-photos --pinentry-mode loopback"
 echo $PGP_SECRET | base64 --decode | gpg  --import
