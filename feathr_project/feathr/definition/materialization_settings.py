ebf113b4952a6ba4d3973b251b926f5e15c0a7d2
enya-yx <enya@7633599-06281.northamerica.corp.microsoft.com>
2022-09-16 17:19:38 +0800

    revert change by mistake

--- feathr_project/feathr/definition/materialization_settings.py
+++ feathr_project/feathr/definition/materialization_settings.py
@@ -5,7 +5,7 @@ import math
 
 
 class BackfillTime:
-    """Time range to materialize/backfill feature data. Please refer to https://linkedin.github.io/feathr/concepts/materializing-features.html#feature-backfill for a more detailed explanation.
+    """Time range to materialize/backfill feature data. Please refer to https://feathr-ai.github.io/feathr/concepts/materializing-features.html#feature-backfill for a more detailed explanation.
 
     Attributes:
         start: start time of the backfill, inclusive.
