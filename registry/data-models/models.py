06966486ba8685f2a731802e0164b125b2235b8b
Helen Yang <hyingyang@gmail.com>
2022-09-19 19:32:26 -0700

    Update models.py

--- registry/data-models/models.py
+++ registry/data-models/models.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel
-from typing import List, Optional
+from typing import List
 
 """
 This file defines abstract backend data models for feature registry.
@@ -8,46 +8,76 @@ Purpose of this is to decouple backend data models from API specific data models
 For each feature registry provider/implementation, they will extend this abstract
 data models and backend API.
 Diagram of the data models:
-                     +-----------------+
-                     |     Project     |
-                     +-----------------+
-                             |  
-                             |
-                             | 
-                +------------------------------+    
-                |                              |      
-                | 1:n                          | 1:n         
-       +------------------+           +------------------+
-       | FeatureInterface |           |     Anchor       |
-       +------------------+           +------------------+
-               ｜                             ｜        |
-               ｜ 1:n                         ｜1:n     |
-               +------------------------------+        |
-                             ｜                         |
-                             ｜1:n                      | contains
-                      +---------------+           +---------------+
-                      |    Feature    | --------- |    Source     |
-                      +---------------+ contains  +---------------+
-                              |
-                              |
-                      +----------------+
-                      | Transformation |
-                      +----------------+
+                                  +-----------------+
+                                  |     Project     |
+                                  +-----------------+
+                                          |  
+                                          |
+                                          | 
+                            +------------------------------+    
+                            |                              |      
+                            | 1:n                          | 1:n         
+                  +------------------+           +------------------+
+      +---------- |   FeatureName    |           |     Anchor       |
+      |      1:n  +------------------+           +------------------+
+      |                 ｜                             ｜          |
+      |                 ｜ 1:n                         ｜1:n       |
+      |                 +------------------------------+          |
+      |                               ｜                           |
+      |                               ｜1:n                       \|/ has
+      |   +--------------+    +---------------+                 +----------------+
+      |   |Transformation|----|    Feature    | --------------- |   DataSource   |            
+      |   +--------------+ has+---------------+                 +----------------+
+      |                              /|\   |                     /|\           |extends
+      |                               |    |                      |            |
+      |                               |    +----------------------|---------+  |
+      |                               |                           |         |  |
+      |          +--------------------------------------+         |      has| \|/
+      |          |                                      |         |        +----------+
+      |          |extends                               |extends  |has     |  Source  |
+      |    +----------------+                      +-----------------+     +----------+
+      |    | DerivedFeature |                      |  AnchorFeature  |          /|\
+      |    +----------------+                      +-----------------+           |
+      |            |                                                             |
+      |            |                                                             |extends
+      |            |                                                       +------------------+
+      |            +-------------------------------------------------------|   FeatureSource  |
+      |                                                                has +------------------+
+      |                                                                             |
+      +-----------------------------------------------------------------------------|
+                      
+                      
+                      
+                      
+                      
+                      
 """
 
 
-class FeatureValue(BaseModel):
+class FeatureId(BaseModel):
     """
-    A value of a Feature.
-    NOTE: FeatureValue is to be customized in each registry
+    Id for Feature, it's unique ID represents Feature.
     """
     pass
 
 
-class FeatureValueType(BaseModel):
+class FeatureNameId(BaseModel):
     """
-    A value of a Feature.
-    NOTE: FeatureValue is to be customized in each registry
+    Id for FeatureName, it's unique ID represents FeatureName.
+    """
+    pass
+
+
+class AnchorId(BaseModel):
+    """
+    Id for Anchor, it's unique ID represents Anchor.
+    """
+    pass
+
+
+class ProjectId(BaseModel):
+    """
+    Id for Project, it's unique ID represents Project.
     """
     pass
 
@@ -55,7 +85,7 @@ class FeatureValueType(BaseModel):
 class Source(BaseModel):
     """
     Type of FeatureValue.
-    It defines where the feature is extracted from. It can be online, offline, Restli, or other FeatureInterfaces.
+    It defines where the feature is extracted from.
     """
     pass
 
@@ -65,7 +95,7 @@ class DataSource(Source):
 
 
 class FeatureSource(Source):
-    input_feature_interface_ids: List[str]  # List of input feature interface IDs
+    input_feature_name_ids: List[FeatureNameId]  # List of input feature name Keys
     pass
 
 
@@ -73,51 +103,47 @@ class Transformation(BaseModel):
     """
     The transformation of a Feature.
     A transformation function represents the transformation logic to produce feature value from the source of FeatureAnchor
-    NOTE: Transformation is to be customized in each registry
     """
     pass
 
 
 class Feature(BaseModel):
     """
-    Actual implementation of FeatureInterface.
+    Actual implementation of FeatureName.
     An implementation defines where a feature is extracted from (Source) and how it is computed (Transformation).
-    The Source of a feature can be raw data sources (like Rest.li, HDFS) and/or other features.
+    The Source of a feature can be raw data sources and/or other features.
     """
-    feature_interface_id: str  # ID of the feature interface that the feature belongs to
-    id: str
-    name: str
+    id: FeatureId  # Unique ID for Feature
+    feature_name_id: FeatureNameId  # Id of the feature name that the feature belongs to
     source: Source  # Source can be either data source or feature source
     transformation: Transformation  # transformation logic to produce feature value
 
 
 class AnchorFeature(Feature):
     """
-    Feature implementation of FeatureInterface which anchored to a data source.
+    Feature implementation of FeatureName which anchored to a data source.
     """
     source: DataSource
 
 
 class DerivedFeature(Feature):
     """
-    Feature implementation that is derived from other FeatureInterfaces.
+    Feature implementation that is derived from other FeatureNames.
     """
     source: FeatureSource
 
 
-class FeatureInterface(BaseModel):
+class FeatureName(BaseModel):
     """
     Named Feature Interface of FeatureImplementations.
-    Each FeatureInterface is defined by feature producer and interact
-    by feature consumers.Each FeatureInterface enclosed attributes that don't change across
-    implementations. (eg. name, default value, value type, format, status, ownership).
-    One FeatureInterface can have multiple Features for different environments.
+    Each FeatureName is defined by feature producer and interact
+    by feature consumers.Each FeatureName enclosed attributes that don't change across
+    implementations.
+    One FeatureName can have multiple Features for different environments.
     """
-    default_value: Optional[FeatureValue]  # Optional default value
-    id: str
-    value_type: FeatureValueType  # Feature value type
-    project_id: str
-    features: List[Feature]  # List of features that the FeatureInterface has
+    id: FeatureNameId  # unique ID for FeatureName, used to extract data for current FeatureName
+    project_id: ProjectId  # ID of the project the FeatureName belongs to
+    features: List[FeatureId]  # List of ids of feature that the FeatureName has
 
 
 class Project(BaseModel):
@@ -125,9 +151,9 @@ class Project(BaseModel):
     Group of FeatureInterfaces. It can be a project the team is working on,
     or a namespace which related FeatureInterfaces have.
     """
-    id: str
-    name: str
-    feature_interfaces: List[FeatureInterface]  # List of feature interfaces that the project has
+    id: ProjectId  # Unique ID of the project.
+    feature_names: List[FeatureNameId]  # List of feature name ids that the project has
+    anchor_ids: List[AnchorId]   # List of Anchor ids that the project has
 
 
 class Anchor(BaseModel):
@@ -136,7 +162,7 @@ class Anchor(BaseModel):
     This is mainly used by feature producer gather information about DataSource
     and FeatureImplementations associated with the DataSource.
     """
-    id: str
-    project_id: str  # ID of Project that the anchor belongs to
+    id: AnchorId  # Unique ID for Anchor
+    project_id: ProjectId  # ID of Project that the anchor belongs to
     source: DataSource  # data source of the Anchor
-    anchor_features: List[AnchorFeature]  # List of anchor features that the anchor has
+    anchor_features: List[FeatureId]  # List of anchor features that the anchor has
