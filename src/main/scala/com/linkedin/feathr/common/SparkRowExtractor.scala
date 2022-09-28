6f82b6f789b1b77abcaa50e09b870f9710a4d896
Jinghui Mo <jmo@linkedin.com>
2022-09-28 16:59:15 -0400

    1. In SparkRowExtractor.scala, add new extractor method which can be extended to do batch preprocess source dataframe into RDD[IndexRecord]. 2. In FeatureTransformation.scala, add logic to extract features from RDD[IndexedRecord]. 3. Improve some error messages.

--- src/main/scala/com/linkedin/feathr/common/SparkRowExtractor.scala
+++ src/main/scala/com/linkedin/feathr/common/SparkRowExtractor.scala
@@ -1,6 +1,8 @@
 package com.linkedin.feathr.common
 
-import org.apache.spark.sql.catalyst.expressions.GenericRowWithSchema
+import org.apache.avro.generic.IndexedRecord
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.DataFrame
 
 /**
  * An extractor trait that provides APIs to transform a Spark GenericRowWithSchema into feature values
@@ -12,12 +14,27 @@ trait SparkRowExtractor {
    * @param datum input row
    * @return list of feature keys
    */
-  def getKeyFromRow(datum: GenericRowWithSchema): Seq[String]
+  def getKeyFromRow(datum: Any): Seq[String]
 
   /**
    * Get the feature value from the row
    * @param datum input row
    * @return A map of feature name to feature value
    */
-  def getFeaturesFromRow(datum: GenericRowWithSchema): Map[String, FeatureValue]
+  def getFeaturesFromRow(datum: Any): Map[String, FeatureValue]
+
+  /**
+   * Whether the Row extractor needs a one-time batch preprocessing before calling
+   * getKeyFromRow() and getFeaturesFromRow().
+   * This is especially useful when users need to convert the source DataFrame
+   * into specific datatype, e.g. Avro GenericRecord or SpecificRecord.
+   */
+  def hasBatchPreProcessing() = false
+
+  /**
+   * One time batch preprocess the input data source into a RDD[_] for feature extraction later
+   * @param df input data source
+   * @return batch preprocessed dataframe, as RDD[IndexedRecord]
+   */
+  def batchPreProcess(df: DataFrame) : RDD[IndexedRecord] = throw new NotImplementedError("Batch preprocess is not implemented")
 }
