588484b4dd73250b82f404a2efe044af4956d4cf
Jinghui Mo <jmo@linkedin.com>
2022-10-05 16:15:17 -0400

    Improve Avro GenericRecord and SpecificRecord based row-level extractor performance (#723)
    * 1. In SparkRowExtractor.scala, add new extractor method which can be extended to do batch preprocess source dataframe into RDD[IndexRecord].
    2. In FeatureTransformation.scala, add logic to extract features from RDD[IndexedRecord].
    3. Improve some error messages.

--- src/main/scala/com/linkedin/feathr/common/SparkRowExtractor.scala
+++ src/main/scala/com/linkedin/feathr/common/SparkRowExtractor.scala
@@ -1,7 +1,5 @@
 package com.linkedin.feathr.common
 
-import org.apache.spark.sql.catalyst.expressions.GenericRowWithSchema
-
 /**
  * An extractor trait that provides APIs to transform a Spark GenericRowWithSchema into feature values
  */
@@ -12,12 +10,12 @@ trait SparkRowExtractor {
    * @param datum input row
    * @return list of feature keys
    */
-  def getKeyFromRow(datum: GenericRowWithSchema): Seq[String]
+  def getKeyFromRow(datum: Any): Seq[String]
 
   /**
    * Get the feature value from the row
    * @param datum input row
    * @return A map of feature name to feature value
    */
-  def getFeaturesFromRow(datum: GenericRowWithSchema): Map[String, FeatureValue]
+  def getFeaturesFromRow(datum: Any): Map[String, FeatureValue]
 }
