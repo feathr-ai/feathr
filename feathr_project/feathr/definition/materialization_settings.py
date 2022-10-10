a8fd8ada9876c59696977a121cf20c04c7f4e032
enya-yx <enya@v-ellinlu-2.fareast.corp.microsoft.com>
2022-10-10 16:32:07 +0800

    quick fix

--- feathr_project/feathr/definition/materialization_settings.py
+++ feathr_project/feathr/definition/materialization_settings.py
@@ -36,7 +36,7 @@ class MaterializationSettings:
                 self.has_hdfs_sink = True
                 sink.aggregation_features = feature_names
             elif isinstance(sink, RedisSink):
-                    sink.aggregation_features = feature_names
+                sink.aggregation_features = feature_names
         self.sinks = sinks
         self.feature_names = feature_names
 
