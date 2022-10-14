0c9bba0505dfb987ea7c61c755a1153faede465f
Chang Yong Lik <51813538+ahlag@users.noreply.github.com>
2022-10-15 08:17:42 +0900

    Added latest deltalake version (#735)
    * Added latest deltalake version
    
    * Changed == to <= for deltalake installation
    
    * Changed <= to >=

--- feathr_project/setup.py
+++ feathr_project/setup.py
@@ -35,8 +35,7 @@ setup(
         "pyarrow<=9.0.0",
         "pyspark>=3.1.2",
         "python-snappy<=0.6.1",
-        # fixing https://github.com/feathr-ai/feathr/issues/687
-        "deltalake<=0.5.8",
+        "deltalake>=0.6.2",
         "graphlib_backport<=1.0.3",
         "protobuf==3.*",
         "confluent-kafka<=1.9.2",
