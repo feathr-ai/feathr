f3b8b8c704f2ce68d774bb112295450f9ed55f46
Boli Guan <ifendoe@gmail.com>
2022-10-18 23:16:33 +0800

    Update role-management page in UI (#751)

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./app";
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
 import "antd/dist/antd.min.css";
 import "./index.less";
 import "./site.css";
+
+dayjs.extend(utc);
 
 ReactDOM.render(
   <React.StrictMode>
--- ui/src/pages/management/roleManagement.tsx
+++ ui/src/pages/management/roleManagement.tsx
@@ -1,15 +1,30 @@
-import React from "react";
-import { Card, Typography } from "antd";
-import RoleManagementForm from "../../components/roleManagementForm";
+import React, { useState } from "react";
+import { Card, Typography, Space, Alert } from "antd";
+import RoleForm from "./components/RoleForm";
 
 const { Title } = Typography;
 
 const RoleManagement = () => {
+  const [showAlert, setShowAlert] = useState<boolean>(false);
+
+  const handleRole = (isAdmin: boolean) => {
+    setShowAlert(!isAdmin);
+  };
+
   return (
     <div className="page">
       <Card>
-        <Title level={3}>Role Management</Title>
-        <RoleManagementForm isNew={true} editMode={true} />
+        <Space direction="vertical" style={{ width: "100%" }}>
+          {showAlert && (
+            <Alert
+              type="warning"
+              message="You are not admin of any project. Only Project Admins
+      can retrieve management details and grant or delete user roles."
+            />
+          )}
+          <Title level={3}>Role Management</Title>
+        </Space>
+        <RoleForm getRole={handleRole} />
       </Card>
     </div>
   );
