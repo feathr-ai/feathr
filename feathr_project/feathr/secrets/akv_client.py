645b631030ac049d536de0efef57875d2a565bcb
aabbasi-hbo <92401544+aabbasi-hbo@users.noreply.github.com>
2022-11-05 19:08:34 -0700

    Revert "Add abstract class"
    This reverts commit e96459ab45486adfbfe84ab67ecff55b026c391f.

--- feathr_project/feathr/secrets/akv_client.py
+++ feathr_project/feathr/secrets/akv_client.py
@@ -1,32 +1,31 @@
 from azure.keyvault.secrets import SecretClient
+from azure.identity import DefaultAzureCredential
 from loguru import logger
 from azure.core.exceptions import ResourceNotFoundError
-from feathr.secrets.abc import FeathrSecretsManagementClient
 
+class AzureKeyVaultClient:
+    def __init__(self, akv_name: str):
+        self.akv_name = akv_name
+        self.secret_client = None
 
-class AzureKeyVaultClient(FeathrSecretsManagementClient):
-    def __init__(self, secret_namespace: str, secret_client: SecretClient = None):
-        if isinstance(secret_client, SecretClient):
-            self.secret_client = secret_client
-        else:
-            raise RuntimeError("You need to pass an azure.keyvault.secrets.SecretClient instance.")
-
-    def get_feathr_secret(self, secret_name: str) -> str:
+    def get_feathr_akv_secret(self, secret_name: str):
         """Get Feathr Secrets from Azure Key Vault. Note that this function will replace '_' in `secret_name` with '-' since Azure Key Vault doesn't support it
 
         Returns:
-            str: returned secret from secret management service
+            _type_: _description_
         """
+        if self.secret_client is None:
+            self.secret_client = SecretClient(
+                vault_url = f"https://{self.akv_name}.vault.azure.net",
+                credential=DefaultAzureCredential()
+            )
         try:
             # replace '_' with '-' since Azure Key Vault doesn't support it
-            variable_replaced = secret_name.replace('_', '-')  # .upper()
-            logger.info('Fetching the secret {} from Key Vault {}.',
-                        variable_replaced, self.secret_client.vault_url)
+            variable_replaced = secret_name.replace('_','-') #.upper()
+            logger.info('Fetching the secret {} from Key Vault {}.', variable_replaced, self.akv_name)
             secret = self.secret_client.get_secret(variable_replaced)
-            logger.info('Secret {} fetched from Key Vault {}.',
-                        variable_replaced, self.secret_client.vault_url)
+            logger.info('Secret {} fetched from Key Vault {}.', variable_replaced, self.akv_name)
             return secret.value
-        except ResourceNotFoundError:
-            logger.error(
-                f"Secret {secret_name} cannot be found in Key Vault {self.secret_client.vault_url}.")
+        except ResourceNotFoundError as e:
+            logger.error(f"Secret {secret_name} cannot be found in Key Vault {self.akv_name}.")
             raise
--- feathr_project/feathr/secrets/aws_secretmanager.py
+++ feathr_project/feathr/secrets/aws_secretmanager.py
@@ -1,33 +1,36 @@
 from loguru import logger
 import json
-from feathr.secrets.abc import FeathrSecretsManagementClient
-import botocore
+from feathr.secrets.abc import SecretManagementClient
+
 from botocore.exceptions import ClientError
-from aws_secretsmanager_caching.secret_cache import SecretCache
 
 
-class AWSSecretManagerClient(FeathrSecretsManagementClient):
-    def __init__(self, secret_namespace: str = None, secret_client: SecretCache = None):
-        self.secret_id = secret_namespace
-        self.secret_namespace = secret_namespace
-
-        # make sure secret_client is a SecretCache type
-        if not isinstance(secret_client, SecretCache):
-            raise RuntimeError(
-                "You need to pass a aws_secretsmanager_caching.secret_cache.SecretCache instance. Please refer to https://docs.aws.amazon.com/secretsmanager/latest/userguide/retrieving-secrets_cache-python.html for more details.")
+class AWSSecretManagerClient(SecretManagementClient):
+    def __init__(self, secret_name: str, secret_client):
+        self.secret_name = secret_name
+        if secret_client.meta.service_model.service_name != "secretsmanager":
+            raise RuntimeError("secret_client need to be a secretsmanager")
         else:
             self.secret_client = secret_client
 
     def get_feathr_secret(self, secret_string: str):
-        """Get Feathr Secrets from AWS Secrets manager. It's also recommended that the client passes a cache objects to reduce cost.
-        See more details here: https://docs.aws.amazon.com/secretsmanager/latest/userguide/retrieving-secrets_cache-python.html
+        """Get Feathr Secrets from Azure Key Vault. Note that this function will replace '_' in `secret_string` with '-' since Azure Key Vault doesn't support it
         """
+        if self.secret_client is None:
+            raise RuntimeError(
+                "You need to initialize a boto3 secretmanager class and pass it to Feathr")
         try:
-            get_secret_value_response = self.secret_client.get_secret_string(self.secret_namespace)
-            # result is in str format, so we need to load it as a dict
-            secret = json.loads(get_secret_value_response)
-            return secret[secret_string]
+            get_secret_value_response = self.secret_client.get_secret_value(
+                SecretId=secret_string
+            )
+            if 'SecretString' in get_secret_value_response:
+                secret = json.loads(get_secret_value_response['SecretString'])
+                return secret[secret_string]
+            else:
+                raise RuntimeError("Only string format is supported")
         except KeyError as e:
             logger.error(
-                f"Secret {secret_string} cannot be found in secretsmanager {self.secret_id}.")
+                f"Secret {secret_string} cannot be found in secretsmanager {self.secret_name}.")
+            raise e
+        except ClientError as e:
             raise e
