9a14cb15afad218ed9b10646a228246143a46aa5
Yuqing Wei <weiyuqing021@outlook.com>
2022-11-09 11:30:51 +0800

    move maven version config to version
    Signed-off-by: Yuqing Wei <weiyuqing021@outlook.com>

--- feathr_project/feathr/version.py
+++ feathr_project/feathr/version.py
@@ -1,1 +1,10 @@
 __version__ = "0.9.0-rc2"
+
+def get_version():
+    return __version__
+
+# Decouple Feathr MAVEN Version from Feathr Python SDK Version
+import os
+def get_maven_artifact():
+    maven_version = os.environ.get("FEATHR_MAVEN_VERSION", __version__)
+    return f"com.linkedin.feathr:feathr_2.12:{maven_version}"
