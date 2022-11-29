1ca7b9215d48981ba3339628f8e4dff56b244ddd
Jun Ki Min <42475935+loomlike@users.noreply.github.com>
2022-11-29 03:48:47 +0000

    Add feature embedding notebook test
    Signed-off-by: Jun Ki Min <42475935+loomlike@users.noreply.github.com>

--- feathr_project/test/samples/test_notebooks.py
+++ feathr_project/test/samples/test_notebooks.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from tempfile import TemporaryDirectory
+import yaml
 
 import pytest
 try:
@@ -19,6 +20,7 @@ SAMPLES_DIR = (
 )
 NOTEBOOK_PATHS = {
     "nyc_taxi_demo": str(SAMPLES_DIR.joinpath("nyc_taxi_demo.ipynb")),
+    "feature_embedding": str(SAMPLES_DIR.joinpath("feature_embedding.ipynb")),
 }
 
 
@@ -52,3 +54,25 @@ def test__nyc_taxi_demo(config_path, tmp_path):
     assert outputs["materialized_feature_values"].data["265"] == pytest.approx([4160., 10000.], abs=1.)
     assert outputs["rmse"].data == pytest.approx(5., abs=2.)
     assert outputs["mae"].data == pytest.approx(2., abs=1.)
+
+
+@pytest.mark.databricks
+def test__feature_embedding(config_path, tmp_path):
+    notebook_name = "feature_embedding"
+    output_notebook_path = str(tmp_path.joinpath(f"{notebook_name}.ipynb"))
+
+    print(f"Running {notebook_name} notebook as {output_notebook_path}")
+
+    conf = yaml.safe_load(Path(config_path).read_text())
+
+    pm.execute_notebook(
+        input_path=NOTEBOOK_PATHS[notebook_name],
+        output_path=output_notebook_path,
+        # kernel_name="python3",
+        parameters=dict(
+            USE_CLI_AUTH=False,
+            REGISTER_FEATURES=False,
+            SPARK_CONFIG__DATABRICKS__WORKSPACE_INSTANCE_URL=conf["spark_config"]["databricks"]["workspace_instance_url"],
+            CLEAN_UP=True,
+        ),
+    )
