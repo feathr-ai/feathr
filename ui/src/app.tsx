f0e107908671daf88486883e205502534c29e95d
Boli Guan <ifendoe@gmail.com>
2023-01-05 17:23:35 +0800

    Disable auth if Feathr is deployed with RBAC set to off (#925)
    Signed-off-by: Boli Guan <ifendoe@gmail.com>
    
    Signed-off-by: Boli Guan <ifendoe@gmail.com>

--- ui/src/app.tsx
+++ ui/src/app.tsx
@@ -1,7 +1,5 @@
 import React from 'react'
 
-import { InteractionType } from '@azure/msal-browser'
-import { MsalAuthenticationTemplate, MsalProvider } from '@azure/msal-react'
 import { Layout } from 'antd'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
@@ -20,54 +18,53 @@ import Projects from '@/pages/Projects'
 import ResponseErrors from '@/pages/ResponseErrors'
 import RoleManagement from '@/pages/RoleManagement'
 
+import AzureMsal from './components/AzureMsal'
 import Header from './components/HeaderBar/header'
 import SideMenu from './components/SiderMenu/siteMenu'
-import { getMsalConfig } from './utils/utils'
 
 const queryClient = new QueryClient()
 
-const msalClient = getMsalConfig()
+const enableRBAC = window.environment?.enableRBAC
+const authEnable: boolean = (enableRBAC ? enableRBAC : process.env.REACT_APP_ENABLE_RBAC) === 'true'
 
 const App = () => {
   return (
-    <MsalProvider instance={msalClient}>
-      <MsalAuthenticationTemplate interactionType={InteractionType.Redirect}>
-        <QueryClientProvider client={queryClient}>
-          <BrowserRouter>
-            <Layout style={{ minHeight: '100vh', position: 'relative' }}>
-              <SideMenu />
-              <Layout style={{ position: 'relative' }}>
-                <Header />
-                <Layout.Content>
-                  <Routes>
-                    <Route index element={<Home />} />
-                    <Route path="/home" element={<Home />} />
-                    <Route path="/projects" element={<Projects />} />
-                    <Route path="/dataSources" element={<DataSources />} />
-                    <Route path="/features" element={<Features />} />
-                    <Route path="/new-feature" element={<NewFeature />} />
-                    <Route
-                      path="/projects/:project/features/:featureId"
-                      element={<FeatureDetails />}
-                    />
-                    <Route
-                      path="/projects/:project/dataSources/:dataSourceId"
-                      element={<DataSourceDetails />}
-                    />
-                    <Route path="/projects/:project/lineage" element={<ProjectLineage />} />
-                    <Route path="/jobs" element={<Jobs />} />
-                    <Route path="/monitoring" element={<Monitoring />} />
-                    <Route path="/management" element={<Management />} />
-                    <Route path="/role-management" element={<RoleManagement />} />
-                    <Route path="/responseErrors/:status/:detail" element={<ResponseErrors />} />
-                  </Routes>
-                </Layout.Content>
-              </Layout>
+    <AzureMsal enable={authEnable}>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <Layout style={{ minHeight: '100vh', position: 'relative' }}>
+            <SideMenu />
+            <Layout style={{ position: 'relative' }}>
+              <Header />
+              <Layout.Content>
+                <Routes>
+                  <Route index element={<Home />} />
+                  <Route path="/home" element={<Home />} />
+                  <Route path="/projects" element={<Projects />} />
+                  <Route path="/dataSources" element={<DataSources />} />
+                  <Route path="/features" element={<Features />} />
+                  <Route path="/new-feature" element={<NewFeature />} />
+                  <Route
+                    path="/projects/:project/features/:featureId"
+                    element={<FeatureDetails />}
+                  />
+                  <Route
+                    path="/projects/:project/dataSources/:dataSourceId"
+                    element={<DataSourceDetails />}
+                  />
+                  <Route path="/projects/:project/lineage" element={<ProjectLineage />} />
+                  <Route path="/jobs" element={<Jobs />} />
+                  <Route path="/monitoring" element={<Monitoring />} />
+                  <Route path="/management" element={<Management />} />
+                  <Route path="/role-management" element={<RoleManagement />} />
+                  <Route path="/responseErrors/:status/:detail" element={<ResponseErrors />} />
+                </Routes>
+              </Layout.Content>
             </Layout>
-          </BrowserRouter>
-        </QueryClientProvider>
-      </MsalAuthenticationTemplate>
-    </MsalProvider>
+          </Layout>
+        </BrowserRouter>
+      </QueryClientProvider>
+    </AzureMsal>
   )
 }
 
