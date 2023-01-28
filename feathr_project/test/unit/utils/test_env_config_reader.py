453b97f130b457e85297277db2b8cd66f62d0205
Jun Ki Min <42475935+loomlike@users.noreply.github.com>
2023-01-28 00:24:05 -0800

    Refactor config helper to be cleaner (#1011)
    * Refactor config helper to be cleaner
    
    Signed-off-by: Jun Ki Min <42475935+loomlike@users.noreply.github.com>
    
    * Clean up docstrings
    
    Signed-off-by: Jun Ki Min <42475935+loomlike@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Jun Ki Min <42475935+loomlike@users.noreply.github.com>

--- feathr_project/test/unit/utils/test_env_config_reader.py
+++ feathr_project/test/unit/utils/test_env_config_reader.py
@@ -15,57 +15,74 @@ test:
     config:
         key: '{TEST_CONFIG_FILE_VAL}'
 """
+TEST_CONFIG_AKV_VAL = "test_akv_val"
 
 
 @pytest.mark.parametrize(
-    "env_value, expected_value",
+    "env_value, config_file_content, akv_value, expected_value",
     [
-        (TEST_CONFIG_ENV_VAL, TEST_CONFIG_ENV_VAL),
-        (None, TEST_CONFIG_FILE_VAL),
+        (TEST_CONFIG_ENV_VAL, TEST_CONFIG_FILE_CONTENT, TEST_CONFIG_AKV_VAL, TEST_CONFIG_ENV_VAL),
+        (None, TEST_CONFIG_FILE_CONTENT, TEST_CONFIG_AKV_VAL, TEST_CONFIG_FILE_VAL),
+        (None, "", TEST_CONFIG_AKV_VAL, TEST_CONFIG_AKV_VAL),
+        (None, "", None, "default"),
     ]
 )
 def test__envvariableutil__get(
     mocker: MockerFixture,
     env_value: str,
+    config_file_content: str,
+    akv_value: str,
     expected_value: str,
 ):
-    """Test `get` method if it returns the correct value.
+    """Test `get` method if it returns the expected value.
     """
-    if env_value:
-        mocker.patch.object(feathr.utils._env_config_reader.os, "environ", {TEST_CONFIG_KEY: env_value})
-
+    # Mock env variables
+    mocker.patch.object(feathr.utils._env_config_reader.os, "environ", {
+        TEST_CONFIG_KEY: env_value,
+        "secrets__azure_key_vault__name": "test_akv_name",
+    })
+    # Mock AKS
+    mocker.patch.object(
+        feathr.utils._env_config_reader.AzureKeyVaultClient,
+        "get_feathr_akv_secret",
+        return_value=akv_value,
+    )
+    # Generate config file
     f = NamedTemporaryFile(delete=True)
-    f.write(TEST_CONFIG_FILE_CONTENT.encode())
+    f.write(config_file_content.encode())
     f.seek(0)
     env_config = EnvConfigReader(config_path=f.name)
-    assert env_config.get(TEST_CONFIG_KEY) == expected_value
+
+    assert env_config.get(TEST_CONFIG_KEY, default="default") == expected_value
 
 
 @pytest.mark.parametrize(
-    "env_value, expected_value",
+    "env_value, akv_value, expected_value",
     [
-        (TEST_CONFIG_ENV_VAL, TEST_CONFIG_ENV_VAL),
-        (None, None),
-        (TEST_CONFIG_ENV_VAL, TEST_CONFIG_ENV_VAL),
+        (TEST_CONFIG_ENV_VAL, TEST_CONFIG_AKV_VAL, TEST_CONFIG_ENV_VAL),
+        (None, TEST_CONFIG_AKV_VAL, TEST_CONFIG_AKV_VAL),
+        (None, None, None),
     ]
 )
 def test__envvariableutil__get_from_env_or_akv(
     mocker: MockerFixture,
     env_value: str,
+    akv_value: str,
     expected_value: str,
 ):
-    """Test `get_from_env_or_akv` method if it returns the environment variable regardless of `use_env_vars` argument.
-
-    Args:
-        mocker (MockerFixture): _description_
-        env_value (str): _description_
-        expected_value (str): _description_
+    """Test `get_from_env_or_akv` method if it returns the expected value.
     """
-    if env_value:
-        mocker.patch.object(feathr.utils._env_config_reader.os, "environ", {TEST_CONFIG_KEY: env_value})
+    # Mock env variables
+    mocker.patch.object(feathr.utils._env_config_reader.os, "environ", {
+        TEST_CONFIG_KEY: env_value,
+        "secrets__azure_key_vault__name": "test_akv_name",
+    })
+    # Mock AKS
+    mocker.patch.object(
+        feathr.utils._env_config_reader.AzureKeyVaultClient,
+        "get_feathr_akv_secret",
+        return_value=akv_value,
+    )
 
-    f = NamedTemporaryFile(delete=True)
-    f.write(TEST_CONFIG_FILE_CONTENT.encode())
-    f.seek(0)
-    env_config = EnvConfigReader(config_path=f.name)
+    env_config = EnvConfigReader(config_path="")
     assert env_config.get_from_env_or_akv(TEST_CONFIG_KEY) == expected_value
