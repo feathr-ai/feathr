a7d5b20343458faa3bfb03f4381c1e47be5bd988
Enya-Yx <108409954+enya-yx@users.noreply.github.com>
2023-02-14 11:05:30 +0800

    Feature names conflicts check and auto-correction (#1024)
    * Check feature name conflicts with data set column names
    * Support auto-correction on feature names conflicts in Scala src
    * Expose auto-correction from python client side 
    * Add test cases, docs and workflow image

--- feathr-impl/src/main/scala/com/linkedin/feathr/offline/config/FeatureJoinConfig.scala
+++ feathr-impl/src/main/scala/com/linkedin/feathr/offline/config/FeatureJoinConfig.scala
@@ -283,8 +283,9 @@ case class DateTimeRange(startTime: LocalDateTime, endTime: LocalDateTime)
  * Join config settings, consists of [[ObservationDataTimeSettings]] class, and [[JoinTimeSettings]] class (both optional).
  * @param observationDataTimeSetting  Settings which have parameters specifying how to load the observation data.
  * @param joinTimeSetting Settings which have parameters specifying how to join the observation data with the feature data.
+ * @param conflictsAutoCorrectionSetting Settings which have parameters specifying how to solve feature names conflicts with dataset automatically
  */
-private[feathr] case class JoinConfigSettings(observationDataTimeSetting: Option[ObservationDataTimeSetting], joinTimeSetting: Option[JoinTimeSetting]) {}
+private[feathr] case class JoinConfigSettings(observationDataTimeSetting: Option[ObservationDataTimeSetting], joinTimeSetting: Option[JoinTimeSetting], conflictsAutoCorrectionSetting: Option[ConflictsAutoCorrectionSetting]) {}
 
 /**
  * ObservationDataTimeSetting Definition class with parameters required to load time partitioned observation data used for the feature join.
@@ -307,6 +308,14 @@ private[offline] case class ObservationDataTimeSetting(dateParam: DateParam, tim
  */
 @JsonDeserialize(using = classOf[JoinTimeConfigSettingDefinitionDeserializer])
 private[offline] case class JoinTimeSetting(timestampColumn: TimestampColumn, simulateTimeDelay: Option[Duration], useLatestFeatureData: Boolean)
+
+/**
+ * ConflictsAutoCorrectionSetting object. This object contains parameters related to auto correct name conflicts among feature names and dataset.
+ * @param renameFeatureList If rename feature list. 'False' by default which means to rename dataset
+ * @param suffix            Suffix used to rename conflicted names
+ */
+@JsonDeserialize(using = classOf[ConflictsAutoCorrectionSettingDeserializer])
+private[offline] case class ConflictsAutoCorrectionSetting(renameFeatureList: Boolean, suffix: String)
 
 /**
  * Timestamp column object
