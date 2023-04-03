d844f039bed6ad9e857b6f8c56492f8e7ac00c9a
Jinghui Mo <jmo@linkedin.com>
2023-04-03 12:39:55 -0400

    Add Feathr chat bot in the notebook(Experimental, powered by ChatGPT) (#1132)
    * ChatGPT integration
    
    * Delay version bump

---
+++ feathr_project/feathr/chat/notebook_utils.py
@@ -0,0 +1,23 @@
+from IPython.core.getipython import get_ipython
+import re
+
+def create_new_cell(contents):
+    shell = get_ipython()
+    payload = dict(
+        source='set_next_input',
+        text=contents,
+        replace=False,
+    )
+    shell.payload_manager.write_payload(payload, single=False)
+    
+
+def extract_code_from_string(input_str, lang='python'):
+    """Extract the code block for a given language"""
+    pattern = fr'```({lang}|\s*)\n(.*?)\n```'
+    # Use the re.findall() function to extract the code block
+    match = re.search(pattern, input_str, re.DOTALL)
+    # Check if any code block was found
+    if match:
+        return match.group(2)
+    else:
+        return ""
