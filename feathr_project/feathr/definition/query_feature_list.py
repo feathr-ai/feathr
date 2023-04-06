eb1ed7ad5c4b92f76d44028221338a65b58914c9
aabbasi-hbo <92401544+aabbasi-hbo@users.noreply.github.com>
2023-04-06 15:20:55 -0700

    Update query_feature_list.py

--- feathr_project/feathr/definition/query_feature_list.py
+++ feathr_project/feathr/definition/query_feature_list.py
@@ -12,19 +12,24 @@ class FeatureQuery(HoconConvertible):
         feature_list: a list of feature names
         key: key of `feature_list`, all features must share the same key
         """
-    def __init__(self, feature_list: List[str], key: Optional[Union[TypedKey, List[TypedKey]]] = None) -> None:
+    def __init__(self, feature_list: List[str], key: Optional[Union[TypedKey, List[TypedKey]]] = None, override_time_delay: Optional[str] = None) -> None:
         self.key = key
         if isinstance(key, TypedKey):
             self.key = [key]
         self.feature_list = feature_list
+        if override_time_delay:
+            self.overrideTimeDelay = override_time_delay
 
     def to_feature_config(self) -> str:
         tm = Template("""
             {
                 key: [{{key_columns}}]
                 featureList: [{{feature_names}}]
+                {% if query.overrideTimeDelay is defined %}
+                overrideTimeDelay: "{{query.overrideTimeDelay}}"
+                {% endif %}
             }
         """)
         key_columns = ", ".join(k.key_column for k in self.key) if self.key else "NOT_NEEDED"
         feature_list = ", ".join(f for f in self.feature_list)
-        return tm.render(key_columns = key_columns, feature_names = feature_list)
+        return tm.render(key_columns = key_columns, feature_names = feature_list, query=self)
